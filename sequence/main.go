b0f4e7b973215b1069a6fc83993385e31ae5d2d2
Gergely Brautigam <gergely@arangodb.com>
2019-11-12 08:48:27 +0100

    Added some comment.

--- sequence/main.go
+++ sequence/main.go
@@ -5,6 +5,14 @@ import (
 	"strings"
 )
 
+// Chop up the string as we search it and chop up the search string as well
+// once the first character matched. If the first character matched
+// we only have to further look for the rest of them to match.
+// Once there is only one character left in the search string we can just
+// see if it occurs in the string. If it does we know our sequence is complete
+// so we can increase the count by one.
+// Once that happens, the recurisve call unvineds and continues with the next
+// character in the tail of the string until the search string is empty.
 func searchString(s, search string) (count int) {
 	if len(search) == 1 {
 		if strings.Contains(s, search) {
