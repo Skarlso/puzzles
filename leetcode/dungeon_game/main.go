c51b43c1965b9a831fd4377e0a2a547ac0996735
Gergely Brautigam <skarlso777@gmail.com>
2019-11-17 09:09:32 +0100

    trying to memory optimize

--- leetcode/dungeon_game/main.go
+++ leetcode/dungeon_game/main.go
@@ -10,8 +10,9 @@ type coord struct {
 }
 
 type point struct {
-	c    coord
-	path []coord
+	c          coord
+	path       []coord
+	stringPath string
 }
 
 func (c coord) String() string {
@@ -22,39 +23,41 @@ func move(grid [][]int, p point) []point {
 	ret := make([]point, 0)
 	if p.c.x+1 < len(grid[p.c.y]) {
 		c := coord{x: p.c.x + 1, y: p.c.y}
-		path := make([]coord, len(p.path))
-		copy(path, p.path)
-		path = append(path, c)
-		ret = append(ret, point{c: c, path: path})
+		//path := make([]coord, len(p.path))
+		//copy(path, p.path)
+		//path = append(path, c)
+		ret = append(ret, point{c: c, stringPath: p.stringPath + "R"})
 	}
 	if p.c.y+1 < len(grid) {
 		c := coord{x: p.c.x, y: p.c.y + 1}
-		path := make([]coord, len(p.path))
-		copy(path, p.path)
-		path = append(path, c)
-		ret = append(ret, point{c: c, path: path})
+		//path := make([]coord, len(p.path))
+		//copy(path, p.path)
+		//path = append(path, c)
+		ret = append(ret, point{c: c, stringPath: p.stringPath + "D"})
 	}
 	return ret
 }
 
-func findAllPath(dungeon [][]int) [][]coord {
+func findAllPath(dungeon [][]int) []string {
 	c := coord{0, 0}
-	start := point{c: c, path: []coord{c}}
+	start := point{c: c, path: []coord{c}, stringPath: "S"}
 	// If we use seen here, we will not get all the paths...
 	//seen := make(map[point]bool)
 	path := []point{start}
 	goal := coord{y: len(dungeon) - 1, x: len(dungeon[0]) - 1}
-	allPaths := make([][]coord, 0)
+	//allPaths := make([][]coord, 0)
+	allStringPath := make([]string, 0)
 	for len(path) > 0 {
 		var current point
 		current, path = path[0], path[1:]
 		if current.c == goal {
-			allPaths = append(allPaths, current.path)
+			//allPaths = append(allPaths, current.path)
+			allStringPath = append(allStringPath, current.stringPath)
 		}
 		paths := move(dungeon, current)
 		path = append(path, paths...)
 	}
-	return allPaths
+	return allStringPath
 }
 
 func calculateMinimumHP(dungeon [][]int) int {
@@ -62,13 +65,21 @@ func calculateMinimumHP(dungeon [][]int) int {
 	return findMinimumHP(allPaths, dungeon)
 }
 
-func findMinimumHP(allPaths [][]coord, dungeon [][]int) int {
+func findMinimumHP(allPaths []string, dungeon [][]int) int {
 	minAge := math.MaxInt64
 	for _, p := range allPaths {
 		startingHP := 1
-		sum := 0
+		var (
+			x, y, sum int
+		)
 		for _, c := range p {
-			sum += dungeon[c.y][c.x]
+			switch c {
+			case 'R':
+				x++
+			case 'D':
+				y++
+			}
+			sum += dungeon[y][x]
 			if v := startingHP + sum; v <= 0 {
 				startingHP += (-v) + 1
 			}
