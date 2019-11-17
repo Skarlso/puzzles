c2f5fa81615bc45d597293a8e84b51e78bd1fc27
Gergely Brautigam <skarlso777@gmail.com>
2019-11-17 08:09:46 +0100

    Too much memory right now.

--- leetcode/dungeon_game/main.go
+++ leetcode/dungeon_game/main.go
@@ -1,115 +1,81 @@
 package dungeon_game
 
 import (
-	"container/heap"
 	"fmt"
+	"math"
 )
 
 type coord struct {
 	x, y int
 }
 
+type point struct {
+	c    coord
+	path []coord
+}
+
 func (c coord) String() string {
 	return fmt.Sprintf("x: %d; y: %d\n", c.x, c.y)
-}
-
-type point struct {
-	c        coord
-	index    int
-	priority int
-}
-
-type ppQ []point
-
-func (pq ppQ) Len() int { return len(pq) }
-func (pq ppQ) Less(i, j int) bool {
-	// implement the cost logic here?
-	return pq[i].priority < pq[j].priority
-}
-
-func (pq ppQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *ppQ) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(point)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *ppQ) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
 }
 
 func move(grid [][]int, p point) []point {
 	ret := make([]point, 0)
 	if p.c.x+1 < len(grid[p.c.y]) {
-		ret = append(ret, point{c: coord{x: p.c.x + 1, y: p.c.y}})
+		c := coord{x: p.c.x + 1, y: p.c.y}
+		path := make([]coord, len(p.path))
+		copy(path, p.path)
+		path = append(path, c)
+		ret = append(ret, point{c: c, path: path})
 	}
 	if p.c.y+1 < len(grid) {
-		ret = append(ret, point{c: coord{x: p.c.x, y: p.c.y + 1}})
+		c := coord{x: p.c.x, y: p.c.y + 1}
+		path := make([]coord, len(p.path))
+		copy(path, p.path)
+		path = append(path, c)
+		ret = append(ret, point{c: c, path: path})
 	}
 	return ret
 }
 
-func calculateMinimumHP(dungeon [][]int) int {
-	// find the path that leads to the smallest positive integer ( absolute value smallest ? )
-	// and go through that until the result is 1.
-	// put this whole thing into a loop?
-	knightHP := 0
-	start := point{c: coord{x: 0, y: 0}, priority: 0, index: 0}
-	cameFrom := map[coord]coord{
-		start.c: start.c,
-	}
-	costSoFar := map[coord]int{
-		start.c: 0,
-	}
-	knightHP += dungeon[start.c.y][start.c.x]
-	maxY := len(dungeon)
-	maxX := len(dungeon[maxY-1])
-	goal := coord{x: maxX - 1, y: maxY - 1}
-	path := make(ppQ, 1)
-	path[0] = start
-	heap.Init(&path)
-	for path.Len() > 0 {
-		current := path.Pop().(point)
-		knightHP += dungeon[current.c.y][current.c.x]
+func findAllPath(dungeon [][]int) [][]coord {
+	c := coord{0, 0}
+	start := point{c: c, path: []coord{c}}
+	// If we use seen here, we will not get all the paths...
+	//seen := make(map[point]bool)
+	path := []point{start}
+	goal := coord{y: len(dungeon) - 1, x: len(dungeon[0]) - 1}
+	allPaths := make([][]coord, 0)
+	for len(path) > 0 {
+		var current point
+		current, path = path[0], path[1:]
 		if current.c == goal {
-			fmt.Printf("cost so far: %d\n", costSoFar[current.c])
-			continue
-			//break
+			allPaths = append(allPaths, current.path)
 		}
-		// move needs to add in paths so it is tracked what route has been taken so far.
 		paths := move(dungeon, current)
-		for _, next := range paths {
-			newCost := costSoFar[current.c] + dungeon[next.c.y][next.c.x]
-			if _, ok := cameFrom[next.c]; !ok || newCost < costSoFar[next.c] {
-				cameFrom[next.c] = current.c
-				next.priority = newCost
-				costSoFar[next.c] = newCost
-				path.Push(next)
+		path = append(path, paths...)
+	}
+	return allPaths
+}
+
+func calculateMinimumHP(dungeon [][]int) int {
+	allPaths := findAllPath(dungeon)
+	return findMinimumHP(allPaths, dungeon)
+}
+
+func findMinimumHP(allPaths [][]coord, dungeon [][]int) int {
+	minAge := math.MaxInt64
+	for _, p := range allPaths {
+		startingHP := 1
+		sum := 0
+		for _, c := range p {
+			sum += dungeon[c.y][c.x]
+			if v := startingHP + sum; v <= 0 {
+				startingHP += (-v) + 1
 			}
 		}
+		if startingHP < minAge {
+			minAge = startingHP
+		}
 	}
-
-	p := make([]coord, 0)
-	current := goal
-	sum := 0
-	for current != start.c {
-		p = append(p, current)
-		sum += dungeon[current.y][current.x]
-		current = cameFrom[current]
-	}
-	p = append(p, start.c)
-	//sum += dungeon[start.c.y][start.c.x]
-	fmt.Println(p, sum)
-	return 0
+	return minAge
 }
--- leetcode/dungeon_game/main_test.go
+++ leetcode/dungeon_game/main_test.go
@@ -1,18 +1,32 @@
 package dungeon_game
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBasic(t *testing.T) {
-	// -6 is the smallest negative which will be the minimum positive.
-	calculateMinimumHP([][]int{
+	minLife := calculateMinimumHP([][]int{
 		{-2, -3, 3},
 		{-5, -10, 1},
 		{10, 30, -5},
 	})
+	assert.Equal(t, 7, minLife, "minimum life should equal the given value")
+}
 
-	//calculateMinimumHP([][]int{
-	//	{1, 1, 0},
-	//	{0, 0, 0},
-	//	{0, 0, 0},
-	//})
+func Test2Basic(t *testing.T) {
+	minLife := calculateMinimumHP([][]int{
+		{1, -3, 2},
+		{0, -1, 2},
+		{0, 0, -2},
+	})
+	assert.Equal(t, 1, minLife, "minimum life should equal the given value")
+}
+
+func Test3Basic(t *testing.T) {
+	minLife := calculateMinimumHP([][]int{
+		{-3, 5},
+	})
+	assert.Equal(t, 4, minLife, "if the knights life begins with -number he immediately dies")
 }
