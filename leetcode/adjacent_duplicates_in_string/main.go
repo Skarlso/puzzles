e908c5970d4920d288b175e514093f03cc46f4ae
Gergely Brautigam <skarlso777@gmail.com>
2019-11-20 06:33:44 +0100

    Faster than 10% with 230ms and 3.7MB memory is better than 100%

--- leetcode/adjacent_duplicates_in_string/main.go
+++ leetcode/adjacent_duplicates_in_string/main.go
@@ -1,20 +1,25 @@
 package adjacent_duplicates_in_string
 
+/*
+Runtime: 264 ms, faster than 10.64% of Go online submissions for Remove All Adjacent Duplicates in String II.
+Memory Usage: 3.4 MB, less than 100.00% of Go online submissions for Remove All Adjacent Duplicates in String II.
+*/
 func removeDuplicates(s string, k int) string {
 	sb := []byte(s)
-	duplicateFound := false
-main:
 	for {
 		var (
-			removeFrom int
-			removeTo   int
+			removeFrom     int
+			removeTo       int
+			duplicateFound bool
 		)
 		for i := 0; i < len(sb)-1; i++ {
 			if sb[i] == sb[i+1] {
 				removeFrom = i
-				for j := i; j < i+k; j++ {
-					if j < len(sb) && sb[j] == sb[i] {
+				for j := i; j < i+k && j < len(sb); j++ {
+					if sb[j] == sb[i] {
 						removeTo = j
+					} else {
+						break
 					}
 				}
 				if removeTo-removeFrom == k-1 {
@@ -25,9 +30,8 @@ main:
 		}
 
 		if !duplicateFound {
-			break main
+			break
 		}
-		duplicateFound = false
 	}
 	return string(sb)
 }
--- leetcode/adjacent_duplicates_in_string/main_test.go
+++ leetcode/adjacent_duplicates_in_string/main_test.go
@@ -15,3 +15,18 @@ func Test2Basic(t *testing.T) {
 	s := removeDuplicates("deeedbbcccbdaa", 3)
 	assert.Equal(t, "aa", s, "should have removed everything")
 }
+
+func Test3Basic(t *testing.T) {
+	st := "mtaknxdfffffffttttttttttttttttttttttttttttttttttttttttttttttttttfffffffffffffffffffffffffffffffffffffffffffpkrzbadhyvrojrqkptqffpxlkzjvbvbppppppppppbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbpppppppprrrrrrrrrrddddddddddddddddddddddddddddddddddddddddddddddddddrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxttttttttttttttttttttttttttttttttttttttttttttttttttxxxpppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxpppppppppppppppppppppppoktyhzpvnokn"
+	s := removeDuplicates(st, 50)
+	assert.Equal(t, "mtaknxdpkrzbadhyvrojrqkptqffpxlkzjvbvboktyhzpvnokn", s, "should have removed everything")
+}
+
+func BenchmarkBasic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := removeDuplicates("deeedbbcccbdaa", 3)
+		if s != "aa" {
+			b.Fatal("s: ", s)
+		}
+	}
+}
